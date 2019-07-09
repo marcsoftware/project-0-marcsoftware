a91965a8cd5e656a75b85f45cd62dcabc14a15c7
Marc Melcher <marcdavidmelcher@gmail.com>
2019-07-09 12:23:26 -0500

    everything works but error message for not closing connection during runtime

--- src/main/java/com/revature/MakeTable.java
+++ src/main/java/com/revature/MakeTable.java
@@ -2,40 +2,39 @@ package com.revature;
 import java.sql.*;  
 
 class MakeTable{  
-   public static void main(String args[]){  
+//mvn install:install-file -Dfile=c:/db/postgresql-42.2.6.jar -DgroupId=org.postgresql -DartifactId=postgresql -Dversion=14.2.6 -Dpackaging=jar
+public static void main(String[] args) {
 
-      System.out.println("-------------------------------------------------------");
-      System.out.println("-------------------------------------------------------");
-      System.out.println("-------------------------------------------------------");
-      System.out.println("-------------------------------------------------------");
-      System.out.println("-------------------------------------------------------");
+   // https://docs.oracle.com/javase/8/docs/api/java/sql/package-summary.html#package.description
+   // auto java.sql.Driver discovery -- no longer need to load a java.sql.Driver class via Class.forName
 
-   try{  
-      //mvn install:install-file -Dfile=c:/db/postgresql-42.2.6.jar -DgroupId=org.postgresql -DartifactId=postgresql -Dversion=14.2.6 -Dpackaging=jar
-      Connection con=DriverManager.getConnection(   "jdbc:postgresql://localhost:5232/test?user=postgre&password=none");  
-      //here sonoo is database name, root is username and password  
-      
-      
-      
-      
-      con.close();  
-   }catch(Exception e){ 
-      //System.out.println( "mvn install:install-file -Dfile=c:/db/postgresql-42.2.6.jar -DgroupId=org.postgresql -DartifactId=postgresql -Dversion=14.2.6 -Dpackaging=jar";
-      System.out.println(e);
-   }  
-
-
-
+   // register JDBC driver, optional, since java 1.6
+   /*try {
+       Class.forName("org.postgresql.Driver");
+   } catch (ClassNotFoundException e) {
+       e.printStackTrace();
+   }*/
    
-   System.out.println("-------------------------------------------------------");
-   System.out.println("-------------------------------------------------------");
-   System.out.println("-------------------------------------------------------");
-   System.out.println("-------------------------------------------------------");
-   System.out.println("-------------------------------------------------------");
+   // auto close connection
+   try (Connection conn = DriverManager.getConnection(
+           "jdbc:postgresql://localhost:5432/test", "postgres", "none")) {
 
-}  
+       if (conn != null) {
+           System.out.println("Connected to the database!");
+       } else {
+           System.out.println("Failed to make connection!");
+       }
 
-  
+   } catch (SQLException e) {
+       System.err.format("SQL State: %s\n%s", e.getSQLState(), e.getMessage());
+   } catch (Exception e) {
+       e.printStackTrace();
+   }finally{
+      
+   }
+
 
 }
+}
+
 
