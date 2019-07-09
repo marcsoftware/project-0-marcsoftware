207012f09f8e7c49793b316d261e78ece655430c
Marc Melcher <marcdavidmelcher@gmail.com>
2019-07-09 08:20:44 -0500

    fixed error message: but now new error is cant find postgre driver.

--- src/main/java/com/revature/MakeTable.java
+++ src/main/java/com/revature/MakeTable.java
@@ -9,16 +9,30 @@ class MakeTable{
       System.out.println("-------------------------------------------------------");
       System.out.println("-------------------------------------------------------");
       System.out.println("-------------------------------------------------------");
-   try{  
-      Class.forName("com.mysql.jdbc.Driver");  
-      Connection con=DriverManager.getConnection(  
-         "jdbc:mysql://localhost:80/sonoo","root","root");  
-      //here sonoo is database name, root is username and password  
-      Statement stmt=con.createStatement();  
-      ResultSet rs=stmt.executeQuery("select * from emp");  
-      while(rs.next())  
-      System.out.println(rs.getInt(1)+"  "+rs.getString(2)+"  "+rs.getString(3));  
-      con.close();  
-   }catch(Exception e){ System.out.println(e);}  
-   }  
+      try{  
+
+            Connection conn = DriverManager.getConnection( 
+                  "jdbc:postgre://localhost:5232/test?user=postgre&password=none"
+               );
+
+            //String url = "jdbc:postgresql://localhost/test?user=fred&password=secret&ssl=true";
+            //Connection conn = DriverManager.getConnection(url);
+            //Class.forName("com.mysql.jdbc.Driver");  
+            
+            conn.close();  
+
+      }catch(Exception e){ 
+            System.out.println(e);
+            System.out.println("not Connected?");
+      }  
+       
+
+   
+   System.out.println("-------------------------------------------------------");
+   System.out.println("-------------------------------------------------------");
+   System.out.println("-------------------------------------------------------");
+   System.out.println("-------------------------------------------------------");
+   System.out.println("-------------------------------------------------------");
 }  
+
+}
