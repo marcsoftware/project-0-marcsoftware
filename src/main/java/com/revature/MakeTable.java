2a5cbcf5f03dfae3ed5305c863a561d95f58006f
Marc Melcher <marcdavidmelcher@gmail.com>
2019-07-09 10:58:11 -0500

    fixed driver not found error with install command

--- src/main/java/com/revature/MakeTable.java
+++ src/main/java/com/revature/MakeTable.java
@@ -10,15 +10,23 @@ class MakeTable{
       System.out.println("-------------------------------------------------------");
       System.out.println("-------------------------------------------------------");
    try{  
-      Class.forName("com.mysql.jdbc.Driver");  
-      Connection con=DriverManager.getConnection(  
-         "jdbc:mysql://localhost:80/sonoo","root","root");  
+      //mvn install:install-file -Dfile=c:/db/postgresql-42.2.6.jar -DgroupId=org.postgresql -DartifactId=postgresql -Dversion=14.2.6 -Dpackaging=jar
+      Connection con=DriverManager.getConnection(   "jdbc:postgresql://localhost:5232/test?user=postgre&password=none");  
       //here sonoo is database name, root is username and password  
-      Statement stmt=con.createStatement();  
-      ResultSet rs=stmt.executeQuery("select * from emp");  
-      while(rs.next())  
-      System.out.println(rs.getInt(1)+"  "+rs.getString(2)+"  "+rs.getString(3));  
+      
+      
+      
+      
       con.close();  
-   }catch(Exception e){ System.out.println(e);}  
+   }catch(Exception e){ 
+      //System.out.println( "mvn install:install-file -Dfile=c:/db/postgresql-42.2.6.jar -DgroupId=org.postgresql -DartifactId=postgresql -Dversion=14.2.6 -Dpackaging=jar";
+      System.out.println(e);}  
    }  
+
+   
+   System.out.println("-------------------------------------------------------");
+   System.out.println("-------------------------------------------------------");
+   System.out.println("-------------------------------------------------------");
+   System.out.println("-------------------------------------------------------");
+   System.out.println("-------------------------------------------------------");
 }  
