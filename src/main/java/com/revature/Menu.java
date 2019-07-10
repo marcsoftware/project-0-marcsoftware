7239675ebadace158a7db66b3a2c143c83a89fb5
Marc Melcher <marcdavidmelcher@gmail.com>
2019-07-10 17:01:42 -0500

    added infinate loop for user input

--- src/main/java/com/revature/Menu.java
+++ src/main/java/com/revature/Menu.java
@@ -1,10 +1,21 @@
 package com.revature;
+import java.util.Scanner;
 
 public class Menu {
 
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
-        System.out.println("Hello, World");
+        System.out.println("---CLI---");
+
+        Scanner reader = new Scanner(System.in);  
+        
+        String s = "";
+        while(!s.equals("exit")){
+            System.out.print("> ");
+            s = reader.nextLine(); 
+            
+        }
+        reader.close();
     }
 
 }
