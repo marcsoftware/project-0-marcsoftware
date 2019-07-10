6958e4b8a792d7b5b2dc8e64b5ddafb76b83ec26
Marc Melcher <marcdavidmelcher@gmail.com>
2019-07-10 07:17:47 -0500

    fixed runtime error

--- src/main/java/com/revature/MakeTable.java
+++ src/main/java/com/revature/MakeTable.java
@@ -5,22 +5,13 @@ class MakeTable{
 //mvn install:install-file -Dfile=c:/db/postgresql-42.2.6.jar -DgroupId=org.postgresql -DartifactId=postgresql -Dversion=14.2.6 -Dpackaging=jar
     public static void main(String[] args) {
 
-        // https://docs.oracle.com/javase/8/docs/api/java/sql/package-summary.html#package.description
-        // auto java.sql.Driver discovery -- no longer need to load a java.sql.Driver class via Class.forName
-
-        // register JDBC driver, optional, since java 1.6
-        /*try {
-            Class.forName("org.postgresql.Driver");
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        }*/
-        
         // auto close connection
         try (Connection conn = DriverManager.getConnection(
                 "jdbc:postgresql://localhost:5432/test", "postgres", "none")) {
 
             if (conn != null) {
                 System.out.println("Connected to the database!");
+                printTable(conn);
             } else {
                 System.out.println("Failed to make connection!");
             }
@@ -37,6 +28,26 @@ class MakeTable{
 
 
     }
-}
+
+
+    public static void printTable(Connection conn){
+        System.out.println("printing table.");
+        String query = "select * from test";
+         
+        try{
+            Statement stmt;
+            stmt = conn.createStatement(); 
+            
+            ResultSet rs = stmt.executeQuery(query);
+            ResultSetMetaData rsmd = rs.getMetaData();
+
+            System.out.println("done printing table.");
+        }catch(Exception  e){
+            System.err.format("ERROR: \n%s", e.getMessage());
+        }finally{
+
+        }
+    }
+} 
 
 
